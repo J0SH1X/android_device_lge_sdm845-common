4e4048809beb8b9e06824c734924d2238248f933
SGCMarkus <markusornik@gmail.com>
2019-07-28 14:24:01 +0200

    sdm845-common: add and copy qti_whitelist/privapp-permissions-qti

--- sdm845.mk
+++ sdm845.mk
@@ -292,6 +292,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+    $(COMMON_PATH)/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext

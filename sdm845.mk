ed8373eb2b26ce19c4bdd5e7afb00a1a8b23d49d
SGCMarkus <markusornik@gmail.com>
2019-07-28 14:12:07 +0200

    sdm845-common: add extra public libraries

--- sdm845.mk
+++ sdm845.mk
@@ -36,6 +36,10 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 560dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # A/B
 AB_OTA_UPDATER := true
 

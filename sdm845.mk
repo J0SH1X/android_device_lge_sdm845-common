27af5f7a0e7b78687c72d3ee58f75157ba499ec0
SGCMarkus <markusornik@gmail.com>
2019-07-28 14:27:35 +0200

    sdm845-common: build usb and vibrator services

--- sdm845.mk
+++ sdm845.mk
@@ -344,6 +344,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \

f3875e7a6465c74c44e99552a893847562fb6e0c
SGCMarkus <markusornik@gmail.com>
2019-09-13 08:58:56 +0200

    sdm845-common: cleanup config.fs
    remove services we dont have

--- config.fs
+++ config.fs
@@ -40,24 +40,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/imsdatadaemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
-[vendor/bin/imsrcsd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: WAKE_ALARM
-
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
@@ -81,12 +63,6 @@ mode: 0755
 user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
-
-[vendor/bin/sensors.qcom]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
 
 [vendor/bin/sensors.qti]
 mode: 0755

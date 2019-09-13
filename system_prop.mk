56f8668ab95198ab9897b3c25a2a711a408a6310
SGCMarkus <markusornik@gmail.com>
2019-09-13 08:18:57 +0200

    sdm845-common: Kang walleye offsets for GPU composition

--- system_prop.mk
+++ system_prop.mk
@@ -36,6 +36,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 
 
 # Voice assistant
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \

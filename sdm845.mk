dd3ea37666245af0c37d3464c275e0fdd055b26e
SGCMarkus <markusornik@gmail.com>
2019-09-22 17:36:26 +0200

    sdm845-common: build Display HALs from source

--- sdm845.mk
+++ sdm845.mk
@@ -153,16 +153,16 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
+    vendor.qti.hardware.display.allocator@1.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-qti-display  \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    libqdutils \
-    libqservice \
-    libdrm.vendor \
+    gralloc.sdm845 \
+    hwcomposer.sdm845 \
+    memtrack.sdm845 \
+    vendor.display.config@1.7 \
     libtinyxml \
     libvulkan
 
@@ -398,7 +398,9 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl
+    libdisplayconfig \
+    libnl \
+    libqdMetaData.system
 
 #PRODUCT_BOOT_JARS += \
 #    WfdCommon

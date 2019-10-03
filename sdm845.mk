4fa2f227a7147c5417736ac7929763fd677cc602
SGCMarkus <markusornik@gmail.com>
2019-10-03 15:17:54 +0200

    sdm845-common: switch to graphics composer 2.3

--- sdm845.mk
+++ sdm845.mk
@@ -157,8 +157,8 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator@1.0-service \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.3-impl \
+    android.hardware.graphics.composer@2.3-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display  \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \

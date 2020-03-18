9a3a3c955c6c9e85d3b09b68ee35e71ed7780b79
LlamaWithaPC <gregariivanov@gmail.com>
2020-03-18 01:32:20 +0100

    sdm845-common: Doze: Use the correct sensor

--- doze/src/org/lineageos/settings/doze/PocketSensor.java
+++ doze/src/org/lineageos/settings/doze/PocketSensor.java
@@ -46,7 +46,7 @@ public class PocketSensor implements SensorEventListener {
     public PocketSensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = Utils.getSensor(mSensorManager, "android.sensor.proximity");
+        mSensor = Utils.getSensor(mSensorManager, "lge.sensor.pocket");
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
